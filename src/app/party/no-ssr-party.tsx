b2e402211f9f5dd6e16064139bf91994c1b60c98
c-ehrlich <ehrlich.christopher@gmail.com>
2023-09-23 20:50:52 +0200

    remove chat and player-info components

--- src/app/party/no-ssr-party.tsx
+++ src/app/party/no-ssr-party.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { CatVoting } from "./cat-voting";
-import { Chat } from "./chat";
-import { PlayerInfo } from "./player-info";
 
 export default function NoSsrParty() {
   return (
     <main className="p-2 flex flex-col gap-4">
-      <PlayerInfo />
       <CatVoting />
-      <Chat />
     </main>
   );
 }
